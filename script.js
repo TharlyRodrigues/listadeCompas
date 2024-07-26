6550fb77814ca74ff9dee3c3a20e07b0fa7ecb57
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-07-26 20:41:53 -0300

    add funções de apagar os items da lista de compras

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById("item-form");
 const ItemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 function addItem(e) {
   e.preventDefault();
@@ -23,6 +25,8 @@ function addItem(e) {
 
   itemList.appendChild(li);
 
+  checkUI();
+
   ItemInput.value = "";
 }
 
@@ -40,5 +44,37 @@ function createIcon(classes) {
   return icon;
 }
 
+//função para remover os itens
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("tem certeza que quer paga esse item?")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function clearItem() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
 // event listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItem);
+
+checkUI();
